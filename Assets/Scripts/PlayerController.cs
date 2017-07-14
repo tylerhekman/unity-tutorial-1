1a669ae29b7fa1405d18d20ff376d23fde1a6717
Tyler Hekman <tylerhekman@gmail.com>
2017-07-14 02:11:06 -0700

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float speedMultiplier;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 v3 = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(speedMultiplier * v3);
+    }
+    void Update() {
+        Vector3 currentPosition = rb.position;
+        if (Input.GetKey("space") && rb.position.y < .55)
+        {
+            print("jump");
+            rb.AddExplosionForce(300, currentPosition, 100);
+        }
+    }
+}
