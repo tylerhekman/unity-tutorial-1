682dfeee20ac4bc9c186a809476ec1c910595a49
Tyler Hekman <tylerhekman@gmail.com>
2017-07-18 22:21:52 -0700

    add follower

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
     private Rigidbody rb;
 
+    //SPEED
     public float speedMultiplier;
 
+    //JUMP
 	public float jumpMultiplier;
 	private Vector3 jumpVector = new Vector3(0.0f, 2.0f, 0.0f);
 
+    //SCORING
     private int winCount = 8;
-
     private int count;
     public Text countText;
     public Text winText;
+
+    //FOLLOWER
+    public GameObject follower;
+    private int followDistance = 2;
 
     void Start()
     {
@@ -26,20 +32,23 @@ public class PlayerController : MonoBehaviour {
         winText.text = "";
     }
 
-	void FixedUpdate () {
+    void FixedUpdate()
+    {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 v3 = new Vector3(moveHorizontal, 0, moveVertical);
 
         rb.AddForce(speedMultiplier * v3);
+
+        Vector3 velocity = rb.velocity;
+
+        Vector3 behind = rb.position - normalizeVelocity(velocity);
+        follower.transform.position = behind;
     }
     void Update() {
-        Vector3 currentPosition = rb.position;
         if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < .01)
         {
-            print("jump");
-//            rb.AddExplosionForce(300, currentPosition, 100);
 			rb.AddForce(jumpVector * jumpMultiplier, ForceMode.Impulse);
         }
     }
@@ -67,5 +76,12 @@ public class PlayerController : MonoBehaviour {
             winText.text = "You Win!!!";
             countText.text = "";
         }
+    }
+    
+    Vector3 normalizeVelocity(Vector3 velocity)
+    {
+        Vector3 normalizedVelocity = velocity;
+        normalizedVelocity.y = 0;
+        return normalizedVelocity.normalized * followDistance;
     }
 }
