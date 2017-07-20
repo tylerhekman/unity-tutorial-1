828c39e3b3fc10b1ac3b6d95cb311420a571a278
thekman <thekman@cj.com>
2017-07-20 01:10:13 -0700

    follow speed scaling based on distance

--- Assets/Follow.cs
+++ Assets/Follow.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Follow : MonoBehaviour {
 	
 	public GameObject following;
-	public float followSpeed;
 	private float minFollowDistance = 1;
 
 	private Vector3 velocity = Vector3.zero;
@@ -15,8 +14,14 @@ public class Follow : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
-		if (Vector3.Distance(transform.position, following.transform.position) > minFollowDistance) {
-			transform.position = Vector3.SmoothDamp (transform.position, following.transform.position, ref velocity, followSpeed);
+		float distance = Vector3.Distance (transform.position, following.transform.position);
+		if (distance > minFollowDistance) {
+			transform.position = Vector3.SmoothDamp (transform.position, following.transform.position, ref velocity, mapRange(distance, 1.0f, 2.0f, .4f, .2f));
 		}
+	}
+
+	float mapRange (float value, float fromSource, float toSource, float fromTarget, float toTarget)
+	{
+		return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
 	}
 }
