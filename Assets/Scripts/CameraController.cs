040b8f900a302eab063727377def51fe26d1cf23
thekman <thekman@cj.com>
2017-07-21 01:45:36 -0700

    scrolling camera zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,15 +7,23 @@ public class CameraController : MonoBehaviour {
     public GameObject player;
 
     private Vector3 offset;
+	private Vector3 normalizedOffset;
+
+	private Vector3 scrollOffset;
 
 	// Use this for initialization
 	void Start () {
         offset = transform.position - player.transform.position;
+		normalizedOffset = offset.normalized;
 	}
-	
+
+	void Update() {
+		scrollOffset = scrollOffset + (Input.GetAxis ("Mouse ScrollWheel") * normalizedOffset);
+	}
+
 	// Update is called once per frame
 	void LateUpdate () {
-        transform.position = player.transform.position + offset;
+		transform.position = player.transform.position + offset + scrollOffset;
 	}
 
 }
