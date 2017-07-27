75d34ac4d8ed6c9dc3010a4edc7867579f749a65
Tyler Hekman <tylerhekman@gmail.com>
2017-07-26 23:36:48 -0700

    increase collectible rotation speed in dropzone

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -7,12 +7,15 @@ public class Rotator : MonoBehaviour {
     float y0;
     float amplitude = .3F;
     float initialSpeed = 2;
+    float initialRotationMultiplier;
     private float speed;
+    private float rotationMultiplier;
 
     void Start()
     {
         y0 = transform.position.y;
         speed = initialSpeed;
+        initialRotationMultiplier = 1;
     }
 
 	void OnEnable() {
@@ -21,7 +24,7 @@ public class Rotator : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * rotationMultiplier);
         Vector3 bobbingPosition = transform.position;
         bobbingPosition.y = y0 + amplitude * Mathf.Sin(speed * Time.time);
         transform.position = bobbingPosition;
@@ -30,6 +33,7 @@ public class Rotator : MonoBehaviour {
     public void resetSpeed()
     {
         speed = initialSpeed;
+        rotationMultiplier = initialRotationMultiplier;
     }
 
     void OnTriggerEnter(Collider other)
@@ -37,6 +41,7 @@ public class Rotator : MonoBehaviour {
         if(other.gameObject.CompareTag("DropZone"))
         {
             speed = 20;
+            rotationMultiplier = 10;
         }
     }
 }
