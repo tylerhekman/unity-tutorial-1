a1652cbc866caad3dfe9b65f006bbc49c92960dd
thekman <thekman@cj.com>
2017-08-03 22:24:42 -0700

    cursorcontroller returns point in world space

---
+++ Assets/CursorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		int maskOfPlane = 1 << 8;
+		if (Physics.Raycast (ray, out hit, maskOfPlane)) {
+			var position = hit.point;
+			print (position);
+		} else {
+			print("no raycast collision");
+		}
+	}
+}
