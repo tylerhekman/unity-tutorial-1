1cc96e5da17a49f8ce674638a4e56a96c5924411
thekman <thekman@cj.com>
2017-08-10 02:23:33 -0700

    projectiles bounce off walls...and eachother

--- Assets/ProjectileController.cs
+++ Assets/ProjectileController.cs
@@ -11,9 +11,19 @@ public class ProjectileController : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+	}
+
+	void FixedUpdate() {
+		GetComponent<Rigidbody> ().velocity -= Vector3.ClampMagnitude(GetComponent<Rigidbody> ().velocity, 1) * 1 * Time.deltaTime;
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		var bounceVelocity = Vector3.Reflect (-col.relativeVelocity, col.contacts [0].normal);
+		GetComponent<Rigidbody> ().velocity = bounceVelocity;
 	}
 
 	void OnTriggerEnter(Collider other)
