0ffcb949a655e6fc2f030d08e51154f6c1c5660d
Tyler Hekman <tylerhekman@gmail.com>
2017-08-13 23:09:28 -0700

    add tractor beam

---
+++ Assets/Scripts/BeamController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamController : MonoBehaviour {
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        transform.Rotate(180.0f, 0.0f, 0.0f);
+
+    }
+
+    public void tractorBeam(Vector3 position)
+    {
+        if (Input.GetMouseButton(1))
+        {
+            GetComponent<Renderer>().enabled = true;
+            Vector3 heightAdjustedPosition = position;
+            heightAdjustedPosition.y = player.transform.position.y;
+            Vector3 angle = Vector3.Normalize(heightAdjustedPosition - player.transform.position);
+            transform.position = player.transform.position + (angle * 2.5f);
+            transform.LookAt(player.transform.position + (angle * 100.0f));
+        }
+        else {
+            GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour {
+
+    public BeamController beamController;
+	public GunController gunController;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		int maskOfPlane = 1 << 8;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, maskOfPlane))
+        {
+            var position = hit.point;
+            gunController.trackMouse(position);
+            if (Input.GetMouseButtonDown(0))
+            {
+                gunController.fire(position);
+            }
+            beamController.tractorBeam(position);
+        }
+    }
+}
