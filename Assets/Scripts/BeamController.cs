5ff91f9814078d64ade1da70acf63b8a4fe3774c
Tyler Hekman <tylerhekman@gmail.com>
2017-08-13 23:34:50 -0700

    tractor beam attracts collectibles

--- Assets/Scripts/BeamController.cs
+++ Assets/Scripts/BeamController.cs
@@ -6,10 +6,14 @@ public class BeamController : MonoBehaviour {
 
     public GameObject player;
 
+    private Vector3 velocity;
+
     // Use this for initialization
     void Start()
     {
         GetComponent<Renderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        velocity = Vector3.zero;
     }
 
     // Update is called once per frame
@@ -29,6 +33,7 @@ public class BeamController : MonoBehaviour {
         if (Input.GetMouseButton(1))
         {
             GetComponent<Renderer>().enabled = true;
+            GetComponent<Collider>().enabled = true;
             Vector3 heightAdjustedPosition = position;
             heightAdjustedPosition.y = player.transform.position.y;
             Vector3 angle = Vector3.Normalize(heightAdjustedPosition - player.transform.position);
@@ -37,6 +42,14 @@ public class BeamController : MonoBehaviour {
         }
         else {
             GetComponent<Renderer>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.transform.position = Vector3.SmoothDamp(other.gameObject.transform.position, player.transform.position, ref velocity, 0.6f);
         }
     }
 }
